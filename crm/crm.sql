503ec7e19d7e6676535a91e3d579977cf39ad5b3
Takaya-Sugino <takaya.sugino@kellogg.northwestern.edu>
2022-01-18 16:31:45 +0000

    exercise1

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,32 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+
+CREATE TABLE company (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    industry TEXT
+); 
+
+CREATE TABLE contact (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    phone_number TEXT
+    company_id INTEGER
+);
+
+CREATE TABLE activity (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    calls_emails TEXT,
+    contact_id INTEGER,
+    salesperson TEXT,
+    date_time TEXT,
+    note TEXT
+); 
+
+CREATE TABLE industry (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    activity_id INTEGER
+);
